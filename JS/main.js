e3dfaa1e54b7b22848967698ef83d849e97a5eda
Syed Mustafa Ahmed <77160234+syedmustafa026@users.noreply.github.com>
2022-10-03 19:56:10 +0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -49,12 +49,12 @@ function addToDo(event) {
                     toDoDiv.appendChild(newToDo);
                     toDoDiv.appendChild(checked);
                     toDoDiv.appendChild(deleted);
-                    //     `<div class="todo">
-                    //     <div  class="todo-item">
-                    //    <li>${value}</li></div>
-                    //     <button class="check-btn"><i class="fas fa-check"></i></button>
-                    //     <button class="delete-btn"><i class="fas fa-trash"></i></button>
-                    //    </div>`
+            //         `<div class="todo ${savedTheme}-todo">
+            //     <div  class="todo-item">
+            //    <li>${value}</li></div>
+            //     <button class="check-btn"><i class="fas fa-check"></i></button>
+            //     <button class="delete-btn"><i class="fas fa-trash"></i></button>
+            //    </div>`
                 })
             })
             .catch((err) => console.log("err", err))
@@ -104,15 +104,12 @@ function deletecheck(event) {
 }
 
 function getTodos() {
-    const toDoDiv = document.createElement("div");
-    toDoDiv.classList.add('todo', `${savedTheme}-todo`);
-    const newToDo = document.createElement('li');
     // adding to server
     axios.get(`http://localhost:3000/todos`)
         .then((response) => {
             response.data.data.map((value,id) => {
                 toDoList.innerHTML +=
-                    `<div class="todo">
+                    `<div class="todo ${savedTheme}-todo">
                 <div  class="todo-item">
                <li>${value}</li></div>
                 <button class="check-btn"><i class="fas fa-check"></i></button>
@@ -121,17 +118,6 @@ function getTodos() {
             })
         })
         .catch((err) => console.log("err", err))
-
-    // check btn;
-    const checked = document.createElement('button');
-    checked.innerHTML = '<i class="fas fa-check"></i>';
-    checked.classList.add("check-btn", `${savedTheme}-button`);
-    toDoDiv.appendChild(checked);
-    // delete btn;
-    const deleted = document.createElement('button');
-    deleted.innerHTML = '<i class="fas fa-trash"></i>';
-    deleted.classList.add("delete-btn", `${savedTheme}-button`);
-    toDoDiv.appendChild(deleted);
 
 }
 
